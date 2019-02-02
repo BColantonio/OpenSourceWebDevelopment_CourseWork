07bb2b4c2fed0201d37f10a947fd0072eaa3d7a2
BColantonio <BColantonio@email.neit.edu>
2019-02-02 02:38:14 -0500

    no image

--- myapp/routes/about.js
+++ myapp/routes/about.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+var fs = require('fileSystem');
+var image=fs.image('../public/images/punisher.jpg')
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  
   res.render('about', { title: 'about' });
 });
 
