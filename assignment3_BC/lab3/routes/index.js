51bfdc03b4787fde8d7db143fe1ac95c8b03fb83
BColantonio <BColantonio@email.neit.edu>
2019-02-05 00:09:47 -0500

    12:09am 2/5/19

---
+++ assignment3_BC/lab3/routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+const hbs = require('hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+hbs.registerHelper('ptag',(num, message)=>{
+  var msg = '';
+  for(let i=0; i<num; i++)
+  {
+      msg+=`<p>${message}</p>`;
+  }
+
+  return msg;
+});
+
+hbs.registerHelper('opt',()=>{
+  var val = [3,4,5,10,20];
+  var ret = "<select>";
+  for(let i=0; i<val.length; i++)
+  {
+    ret+="<option value="+val[i]+">"+val[i]+"</option>";
+  }
+  ret+="</select>"
+  return ret;
+});
+module.exports = router;
